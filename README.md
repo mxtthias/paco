7927166b8d186a35debbb876473438b2ece59ada
Matthias Nilsson <matthias@baktus.se>
2017-09-07 13:16:25 +0200

    Add initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # paco - The field pair counter
 A PoC CLI app in Rust for KB Labs
+
+## Building
+
+```bash
+$ cargo build --release
+```
+
+## Usage
+
+`paco` takes as input a tsv file with two columns. The program then counts the
+number of occurances of each pair in the file and prints out a sorted list.
+
+```bash
+$ cat pairs.tsv
+a   b
+1   b
+a   c
+1   b
+$ ./paco pairs.tsv
+2:      ["1-b"]
+1:      ["a-b", "a-c"]
+```
+
+The equivalent if using `awk`, `sort`, and `uniq` would be as follows:
+
+```bash
+$ awk '{ print $1 "-" $2 }' pairs.tsv | sort | uniq -c | sort -nr
+```
+
+## License
+
+See [LICENSE](LICENSE).
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+extern crate csv;
+#[macro_use]
+extern crate serde_derive;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use std::error::Error;
+use std::process;
+use std::collections::HashMap;
+use structopt::StructOpt;
+
+/// Count field pairs
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cli")]
+struct Cli {
+    /// Input filename
+    filename: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    a_field: String,
+    b_field: String,
+}
+
+fn count(filename: String) -> Result<(), Box<Error>> {
+    let mut field_pairs = HashMap::new();
+
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .has_headers(false)
+        .from_path(filename)
+        .unwrap();
+
+    for result in reader.deserialize() {
+        let record: Record = result?;
+        let pair = format!("{}-{}", record.a_field, record.b_field);
+        let count = field_pairs.entry(pair).or_insert(0);
+        *count += 1;
+    }
+
+    let mut counts = HashMap::new();
+
+    let mut keys = vec![];
+    for (pair, count) in &field_pairs {
+        let pairs = counts.entry(count).or_insert(vec![]);
+        pairs.push(pair);
+        keys.push(count)
+    }
+
+    keys.sort();
+    keys.dedup();
+    keys.reverse();
+
+    for key in keys {
+        println!("{}:\t{:?}", key, counts.get(key).unwrap());
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    if let Err(err) = count(args.filename) {
+        println!("error counting stats: {}", err);
+        process::exit(1);
+    }
 }
