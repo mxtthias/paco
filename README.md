2a8c13646978534a2c97d4f483a900c0cb25d178
Matthias Nilsson <matthias@baktus.se>
2017-09-07 15:31:20 +0200

    Expand to count two files at the same time
    Parsing is done in a separate thread for each file, while main is
    responsible for collecting the result and counting the pairs.

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ $ cargo build --release
 
 ## Usage
 
-`paco` takes as input a tsv file with two columns. The program then counts the
-number of occurances of each pair in the file and prints out a sorted list.
+`paco` takes as input two tsv files with two columns. The program then counts
+the number of occurances of each pair in the files and prints out a sorted
+list.
 
 ```bash
 $ cat pairs.tsv
@@ -18,15 +19,15 @@ a   b
 1   b
 a   c
 1   b
-$ ./paco pairs.tsv
-2:      ["1-b"]
-1:      ["a-b", "a-c"]
+$ ./paco pairs.tsv pairs.tsv
+4:      ["1-b"]
+2:      ["a-b", "a-c"]
 ```
 
 The equivalent if using `awk`, `sort`, and `uniq` would be as follows:
 
 ```bash
-$ awk '{ print $1 "-" $2 }' pairs.tsv | sort | uniq -c | sort -nr
+$ awk '{ print $1 "-" $2 }' pairs.tsv pairs.tsv | sort | uniq -c | sort -nr
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
 
-use std::error::Error;
-use std::process;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::thread;
+use std::sync::mpsc;
 use structopt::StructOpt;
 
 /// Count field pairs
@@ -15,7 +16,8 @@ use structopt::StructOpt;
 #[structopt(name = "cli")]
 struct Cli {
     /// Input filename
-    filename: String,
+    filename1: String,
+    filename2: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -24,28 +26,65 @@ struct Record {
     b_field: String,
 }
 
-fn count(filename: String) -> Result<(), Box<Error>> {
+fn count(filename: String) -> HashMap<String, u32> {
     let mut field_pairs = HashMap::new();
 
-    let mut reader = csv::ReaderBuilder::new()
+    let reader = csv::ReaderBuilder::new()
         .delimiter(b'\t')
         .has_headers(false)
-        .from_path(filename)
-        .unwrap();
+        .from_path(filename);
+
+    let mut reader = match reader {
+        Ok(csv_reader) => csv_reader,
+        Err(error) => panic!("Could not create csv reader: {:?}", error),
+    };
 
     for result in reader.deserialize() {
-        let record: Record = result?;
+        let record: Record = match result {
+            Ok(r) => r,
+            Err(error) => panic!("Could not deserialize record: {:?}", error),
+        };
         let pair = format!("{}-{}", record.a_field, record.b_field);
         let count = field_pairs.entry(pair).or_insert(0);
         *count += 1;
     }
 
-    let mut counts = HashMap::new();
+    return field_pairs;
+}
 
+fn main() {
+    let args = Cli::from_args();
+    let filename1 = args.filename1;
+    let filename2 = args.filename2;
+
+    let (tx1, rx) = mpsc::channel();
+    let tx2 = tx1.clone();
+
+    thread::spawn(move || {
+        let result = count(filename1);
+        tx1.send(result).unwrap();
+    });
+
+    thread::spawn(move || {
+        let result = count(filename2);
+        tx2.send(result).unwrap();
+    });
+
+    let mut pairs = HashMap::new();
+
+    for received in rx {
+        for (pair, count) in received {
+            let counts = pairs.entry(pair).or_insert(0);
+            *counts += count;
+        }
+    }
+
+    let mut counts = HashMap::new();
     let mut keys = vec![];
-    for (pair, count) in &field_pairs {
-        let pairs = counts.entry(count).or_insert(vec![]);
-        pairs.push(pair);
+
+    for (pair, count) in pairs {
+        let pairs = counts.entry(count).or_insert(HashSet::new());
+        pairs.insert(pair);
         keys.push(count)
     }
 
@@ -54,16 +93,6 @@ fn count(filename: String) -> Result<(), Box<Error>> {
     keys.reverse();
 
     for key in keys {
-        println!("{}:\t{:?}", key, counts.get(key).unwrap());
-    }
-
-    Ok(())
-}
-
-fn main() {
-    let args = Cli::from_args();
-    if let Err(err) = count(args.filename) {
-        println!("error counting stats: {}", err);
-        process::exit(1);
+        println!("{}:\t{:?}", key, counts.get(&key).unwrap());
     }
 }
